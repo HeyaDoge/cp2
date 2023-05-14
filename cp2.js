deff114cc14914ca7d4bee2eb47a919f796391ac
HeyaDoge <97377755+HeyaDoge@users.noreply.github.com>
2023-05-14 20:32:43 +0700

    Add files via upload

---
+++ cp2.js
@@ -0,0 +1,10 @@
+var date = Date();
+var minute = Date.getMinutes();
+var second = Date.getSeconds();
+
+function get() {
+    var timer = new Date();
+    var minute = Date.getMinutes();
+    var second = Date.getSeconds();
+    document.getElementById('timer').innerHTML = timer
+}
